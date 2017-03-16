0bfbe68d339a41b79b16bd90e110fcd8dd6c503b
Akilan Mohan <akilan.mohan@sc.com>
2017-03-16 11:17:14 +0800

    updated script files

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+
+echo "post-install.sh started"
 set -e
 
+echo "post-install.sh step1"
 cd ~/node
+
+echo "post-install.sh step2"
 npm install
 
+echo "post-install.sh step3"
 # setup NODE_ENV
 if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
     export NODE_ENV=$DEPLOYMENT_GROUP_NAME
@@ -16,8 +22,11 @@ if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
     fi
 fi
 
+echo "post-install.sh step4"
 # add node to startup
 hasRc=`grep "su -l $USER" /etc/rc.d/rc.local | cat`
 if [ -z "$hasRc" ]; then
     sudo sh -c "echo 'su -l $USER -c \"cd ~/node;sh ./run.sh\"' >> /etc/rc.d/rc.local"
 fi
+
+echo "post-install.sh completed"
