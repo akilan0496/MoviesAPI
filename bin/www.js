9b8924b31de9357fc26add0301224bc18ff62c13
Akilan <makilanit@gmail.com>
2017-03-16 17:07:49 +0800

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 var app = require('../app');
-console.log("djgvsdgjlhlkjshlkjsahflkjsfdhkljsadfhklsadhfkjlsafn");
+
 //Require our app
 var server = app.listen(8000, function() {
   console.log('Express server listening on port ' + server.address().port);
